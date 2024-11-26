edf91603c85dbb3305092b2274ed8bb761ebaee7
ertix222 <ertix222@gmail.com>
2024-11-26 11:35:12 +0100

    maj des fichiers

---
+++ _printf.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+*
+*
+*
+*
+*/
+
+
+
+/**
+* _printf- 
+*@format:
+*
+*
+*/
+
+int _printf(const char *format, ...)
+
+{
+int i;
+char c;
+va_list (args, n);
+va_start;
+
+if (format == c)
+{
+    
+    va_arg(args, char);
+    write(1, &c, 1);
+    i++;
+}
+
+
+
+return (i);
+}
