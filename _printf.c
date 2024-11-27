a1b2dbdf9bf9a34baa877c023e813f094787366e
Yannis <yannismosca@gmail.com>
2024-11-27 10:20:09 +0000

    Correction des erreurs par rapport au %c et %s

--- _printf.c
+++ _printf.c
@@ -12,8 +12,9 @@ int _printf(const char *format, ...)
 
 {
 	int i = 0; /* Index de la chaine de caracteres format */
+	int count = 0; /* Nombre de caracteres printes */
 	char temp[2]; /* Variable temporaire qui recupere le specificateur, soit format + 1 */
-	int (*func)(va_list) = get_func(temp);
+	int (*func)(va_list);
 	va_list args;
 	va_start(args, format);
 
@@ -24,16 +25,19 @@ int _printf(const char *format, ...)
 			temp[0] = format[i + 1];
 			temp[1] = '\0';
 
-			i += 2; /* Sert a sauter les deux caracteres % + specificateur */
-			i += func(args);
+			func = get_func(temp);
+
+			if (func != NULL)
+			{
+				count += func(args);
+				i += 2; /* Sert a sauter les deux caracteres % + specificateur */
+			}
 		}
-		else /* Sert a imprimer une erreur */
-		{
-			write(1, "This is an error\n", 17);
-			return (-1);
-		}
+		
+		_putchar(format[i]);
 		i++;
+		count++;
 	}
 	va_end(args);
-	return (i);
+	return (count);
 }
